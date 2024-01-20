042f02c61690259e7a91e5d25357ad8baa0230b1
anurag56964 <88320760+anurag56964@users.noreply.github.com>
2024-01-19 22:50:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.delete('/api/movies/:id', async (req, res) => {
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error: 'Internal Server Error0000' });
     }
 });
 
